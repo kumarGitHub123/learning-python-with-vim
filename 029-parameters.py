40cc4f101631383d798d1ee5e584ca1614e2387a
Celmad <manuelalaminosd@protonmail.com>
2019-09-25 14:34:37 +0100

    Added parameters file

---
+++ 029-parameters.py
@@ -0,0 +1,14 @@
+def greet_user(name):
+    print(f'Hi {name}!')
+
+greet_user("Manuel")
+greet_user("Elias")
+
+# name is the parameter
+# "Manuel" is the argument
+
+def greet_full_name(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+
+greet_full_name("Manuel", "Alaminos")
+
