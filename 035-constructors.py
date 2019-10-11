8d749c56603a95fb198a761070342dbae41dc428
Celmad <manuelalaminosd@protonmail.com>
2019-10-11 15:40:48 +0100

    Added more classes examples to Constructors file

--- 035-constructors.py
+++ 035-constructors.py
@@ -15,3 +15,16 @@ class Point:
 point = Point(10, 20)
 print(point.x)
 
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"Hi, I am {self.name}")
+
+john = Person("John")
+john.talk()
+
+bob = Person("Bob")
+bob.talk()
+
